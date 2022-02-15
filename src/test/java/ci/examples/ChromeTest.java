546a58fe97a6725889d8b546c9a86350fe07c508
Dmitriy Kusakin <diman.kusakin@inbox.ru>
2022-02-15 23:49:51 +0300

    Add ChromeTest

---
+++ src/test/java/ci/examples/ChromeTest.java
@@ -0,0 +1,28 @@
+package ci.examples;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class ChromeTest {
+
+    private WebDriver driver;
+
+    @Test
+    void startChrome() {
+        driver = new ChromeDriver();
+        driver.get("https://ya.ru");
+        assertTrue(driver.findElement(By.className("input__box"))
+                        .isDisplayed());
+    }
+
+    @AfterEach
+    void closeDriver() {
+        if (driver != null)
+            driver.close();
+    }
+}
